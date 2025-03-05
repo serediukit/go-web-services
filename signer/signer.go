a764735850d0d1f21f0bdd91d21360dfbf607fcf
serediukit <serediukit@gmail.com>
2025-03-05 19:26:48 +0200

    add changes

--- signer/signer.go
+++ signer/signer.go
@@ -4,54 +4,69 @@ import (
 	"fmt"
 	"runtime"
 	"strconv"
+	"sync"
 )
 
 func ExecutePipeline(jobs ...job) {
 	in := make(chan interface{}, 1)
-	out := make(chan interface{}, 1)
+	out := make(chan interface{}, 100)
 
-	for i, job := range jobs {
+	wg := &sync.WaitGroup{}
+
+	for i, j := range jobs {
+		in = out
+		out = make(chan interface{}, 100)
+
 		fmt.Printf("JOB #%d\n", i)
 		//fmt.Println("CHANNEL IN:", in)
 		//fmt.Println("CHANNEL OUT:", out)
-		go job(in, out)
-		in = out
-		out = make(chan interface{}, 1)
+
+		wg.Add(1)
+		go func(j job, in, out chan interface{}) {
+			defer wg.Done()
+			defer close(out)
+
+			j(in, out)
+		}(j, in, out)
 	}
+
+	wg.Wait()
 }
 
 func SingleHash(in, out chan interface{}) {
-	data := strconv.Itoa((<-in).(int))
-	md5 := DataSignerMd5(data)
-	crc32Data := DataSignerCrc32(data)
-	crc32Md5 := DataSignerCrc32(md5)
-	calc := crc32Data + "~" + crc32Md5
-	fmt.Println("SH calc:", calc)
-	out <- calc
-	runtime.Gosched()
+	for i := range in {
+		data := strconv.Itoa(i.(int))
+		md5 := DataSignerMd5(data)
+		crc32Data := DataSignerCrc32(data)
+		crc32Md5 := DataSignerCrc32(md5)
+		calc := crc32Data + "~" + crc32Md5
+		fmt.Println("SH calc:", calc)
+		out <- calc
+		runtime.Gosched()
+	}
 }
 
 func MultiHash(in, out chan interface{}) {
-	str := ""
-	data := (<-in).(string)
-	for j := 0; j <= 5; j++ {
-		calc := DataSignerCrc32(strconv.Itoa(j) + data)
-		fmt.Println("MH calc:", calc)
-		str += calc
+	for i := range in {
+		str := ""
+		data := (i).(string)
+		for j := 0; j <= 5; j++ {
+			calc := DataSignerCrc32(strconv.Itoa(j) + data)
+			fmt.Println("MH calc:", calc)
+			str += calc
+		}
+		out <- str
+		fmt.Println("MH str:", str)
 	}
-	out <- str
-	fmt.Println("MH str:", str)
-
 }
 
-//func CombineResults(in []string) string {
-//	str := in[0]
-//	for i := 1; i < len(in); i++ {
-//		str += "_" + in[i]
-//	}
-//	fmt.Println("SR res:", str)
-//	return str
-//}
+func CombineResults(in, out chan interface{}) {
+	str := (<-in).(string)
+	for i := range in {
+		str += "_" + i.(string)
+	}
+	out <- str
+}
 
 func main() {
 
@@ -61,10 +76,10 @@ func main() {
 	}
 
 	jobEnd := func(in, out chan interface{}) {
-		fmt.Println("END GET:", <-in)
+		for i := range in {
+			fmt.Println("END GET:", i)
+		}
 	}
 
-	ExecutePipeline(jobStart, SingleHash, MultiHash, jobEnd)
-
-	fmt.Scanln()
+	ExecutePipeline(jobStart, SingleHash, MultiHash, CombineResults, jobEnd)
 }
