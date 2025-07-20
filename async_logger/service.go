2642a3cb88736684016fa5f67bf0bdd278548e83
serediukit <serediukit@gmail.com>
2025-07-21 01:06:32 +0300

    add BizServer implementation

--- async_logger/service.go
+++ async_logger/service.go
@@ -1,4 +1,25 @@
 package main
 
+import "context"
+
 // тут вы пишете код
 // обращаю ваше внимание - в этом задании запрещены глобальные переменные
+type BizServerImpl struct {
+	UnimplementedBizServer
+}
+
+func (s *BizServerImpl) Check(context.Context, *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+
+func (s *BizServerImpl) Add(context.Context, *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+
+func (s *BizServerImpl) Test(context.Context, *Nothing) (*Nothing, error) {
+	return &Nothing{}, nil
+}
+
+func NewBizServer() BizServer {
+	return &BizServerImpl{}
+}
