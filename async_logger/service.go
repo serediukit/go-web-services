96370ec12c30f1cc50ec12624840d2833e44647f
serediukit <serediukit@gmail.com>
2025-07-21 01:20:34 +0300

    add EventSubs implementation

--- async_logger/service.go
+++ async_logger/service.go
@@ -1,6 +1,9 @@
 package main
 
-import "context"
+import (
+	"context"
+	"sync"
+)
 
 // тут вы пишете код
 // обращаю ваше внимание - в этом задании запрещены глобальные переменные
@@ -26,16 +29,70 @@ func NewBizServer() *BizServerImpl {
 
 type AdminServerImpl struct {
 	UnimplementedAdminServer
+
+	subs *EventSubs
 }
 
-func (s *AdminServerImpl) Logging(context.Context, *Nothing) (*Event, error) {
+func (s *AdminServerImpl) Logging(_ *Nothing, srv Admin_LoggingServer) (*Event, error) {
 	return &Event{}, nil
 }
 
-func (s *AdminServerImpl) Statistics(context.Context, *StatInterval) (*Stat, error) {
+func (s *AdminServerImpl) Statistics(_ *StatInterval, srv Admin_StatisticsServer) (*Stat, error) {
 	return &Stat{}, nil
 }
 
 func NewAdminServer() *AdminServerImpl {
 	return &AdminServerImpl{}
+}
+
+type EventSubs struct {
+	id   int
+	subs map[int]chan *Event
+	mux  *sync.Mutex
+}
+
+func NewEventSubs() *EventSubs {
+	return &EventSubs{
+		subs: map[int]chan *Event{},
+		mux:  &sync.Mutex{},
+	}
+}
+
+func (es *EventSubs) NewSub() (int, chan *Event) {
+	es.mux.Lock()
+	defer es.mux.Unlock()
+
+	es.id++
+	es.subs[es.id] = make(chan *Event)
+
+	return es.id, es.subs[es.id]
+}
+
+func (es *EventSubs) RemoveSub(id int) {
+	es.mux.Lock()
+	defer es.mux.Unlock()
+
+	if ch, ok := es.subs[id]; ok {
+		close(ch)
+		delete(es.subs, id)
+	}
+}
+
+func (es *EventSubs) RemoveAll() {
+	es.mux.Lock()
+	defer es.mux.Unlock()
+
+	for _, ch := range es.subs {
+		close(ch)
+	}
+	es.subs = map[int]chan *Event{}
+}
+
+func (es *EventSubs) Notify(e *Event) {
+	es.mux.Lock()
+	defer es.mux.Unlock()
+
+	for _, ch := range es.subs {
+		ch <- e
+	}
 }
